4175df0d393a048f495d86d7f0a07f14ed77c242
PedroMendes414 <pedrooliverio4@gmail.com>
2025-11-04 16:21:23 -0300

    adicionando .env no teste

--- tests/helpers/autenticacao.js
+++ tests/helpers/autenticacao.js
@@ -1,10 +1,13 @@
-const request = require('supertest');
+require('dotenv').config()
+const request = require('supertest')
 const postLogin = require ('../fixtures/postLogin.json')
+
+const BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000';
 
 const obterToken = async (usuario, senha) => {
     const bodyLogin = { ...postLogin}
     // Capturar o token
-    const respostaLogin = await request('http://localhost:3000')
+    const respostaLogin = await request(BASE_URL)
         .post('/users/login')
         .set('Content-Type', 'application/json')
         .send(bodyLogin)
